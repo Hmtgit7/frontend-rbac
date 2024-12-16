9f3b55d1b05123b0ba33abea6c502f0b9b8a08a5
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-12-16 18:55:21 +0530

    setting up routes and add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from './Component/Footer/Footer'
+import Navbar from './Component/Navbar/Navbar'
+import { ThemeProvider } from './Context/ThemeContext'
+import Dashboard from './Pages/Dashboard/Dashboard'
+import ManageUsers from "./Pages/ManageUsers/ManageUser";
+import Permission from "./Pages/ManagerPermission/Permission";
+import ManageRoles from "./Pages/ManagerRoles/ManageRoles";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p>hello</p>
-    </>
+    <ThemeProvider>
+      <div style={{ width: "100%" }}>
+        <Router>
+          {/* Navbar */}
+          <Navbar />
+
+          {/* Main content container */}
+          <div className="container">
+            <Routes>
+              {/* AdminControl Routes */}
+              <Route exact path="/" element={<Dashboard />} />
+              <Route exact path="/manageuser" element={<ManageUsers />} />
+              <Route exact path="/managerole" element={<ManageRoles />} />
+              <Route exact path="/managepermission" element={<Permission />} />
+            </Routes>
+          </div>
+
+          {/* Footer Section */}
+          <Footer />
+        </Router>
+      </div>
+    </ThemeProvider>
   )
 }
 
